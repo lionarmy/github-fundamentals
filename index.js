70415f033aa98bb062fe9b7cd9472e7a81789d37
manav <manavbhikadiya@gmail.com>
2022-01-03 19:01:52 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const fruits  =  new Map([
+    ["apple",500],
+    ["banana",100],
+    ["orange",50],
+])
+
+// console.log(fruits.get("orange"));
+
+fruits.set("dragon_fruit",600);
+
+console.log(fruits);
